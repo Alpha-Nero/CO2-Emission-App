47b5f1c53ab8160435ff9c9cfbd3cdcf24ff20fc
Alpha-Nero <114919406+Alpha-Nero@users.noreply.github.com>
2024-05-25 19:12:57 +0400

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php /**database*/
      define('DB_SERVER','localhost');
-     define('DB_USER','user');
+     define('DB_USER','root');
      define('DB_PASS' ,'password');
      define('DB_NAME', 'alpha_db');
      
